68203c778835944efc91fb64275a106ae3f4487c
adityasharat <adityasharat@gmail.com>
2018-09-21 19:57:02 +0100

    Update the react example app

--- example/src/App.js
+++ example/src/App.js
@@ -7,6 +7,11 @@ export default class App extends Component {
     return (
       <div>
         <h1>Let's Learn Some Math</h1>
+          <ul>
+            <li>
+              <code>2 + 2 = {MathProblems.functions.add(2, 2)}</code>
+            </li>
+          </ul>
         <code>version: {MathProblems.version}</code>
       </div>
     )
