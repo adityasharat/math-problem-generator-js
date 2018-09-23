48b634c9785d1a3930234bb84c8bcf3e9f53a0a7
adityasharat <adityasharat@gmail.com>
2018-09-23 23:57:31 +0100

    use operators

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import functions from './functions';
+import operators from './operators';
 
 const MathProblems = {};
 
 MathProblems.version = '1.0.0';
 MathProblems.functions = functions;
+MathProblems.operators = operators;
 
 export default MathProblems;
