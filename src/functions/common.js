ec4f619bd5b9938dbb492c34da581f7fec30f1b8
adityasharat <adityasharat@gmail.com>
2018-09-23 23:47:47 +0100

    fix subtract and add division

--- src/functions/common.js
+++ src/functions/common.js
@@ -5,13 +5,25 @@ export function add(...args) {
 };
 
 export function sub(...args) {
+  if (args.length === 0) {
+    return 0;
+  }
+  if (args.length === 1) {
+    return 0 - args[0];
+  }
   return args.reduce((diff, x) => {
      return diff - x;
-   }, 0);
+   }, args[0] * 2);
 };
 
 export function mul(...args) {
   return args.reduce((product, x) => {
      return product * x;
+   }, 1);
+};
+
+export function div(...args) {
+  return args.reduce((q, x) => {
+     return q / x;
    }, 1);
 };
