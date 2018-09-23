156c0f13885d9c93b7b9e6804b7d13c9496fc321
adityasharat <adityasharat@gmail.com>
2018-09-23 23:57:36 +0100

    test operators

--- src/index.test.js
+++ src/index.test.js
@@ -4,4 +4,10 @@ describe('MathProblems', () => {
   it('is an object', () => {
     expect(MathProblems).toBeDefined()
   })
+  it('has functions', () => {
+    expect(MathProblems.functions).toBeDefined()
+  })
+  it('has operators', () => {
+    expect(MathProblems.operators).toBeDefined()
+  })
 })
