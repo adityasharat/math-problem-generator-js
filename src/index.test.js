885377c0036d7e83621e13bea2c3924b51c5fca8
adityasharat <adityasharat@gmail.com>
2018-09-26 21:45:43 +0100

    update unit tests

--- src/index.test.js
+++ src/index.test.js
@@ -4,10 +4,7 @@ describe('MathProblems', () => {
   it('is an object', () => {
     expect(MathProblems).toBeDefined()
   })
-  it('has functions', () => {
-    expect(MathProblems.functions).toBeDefined()
-  })
-  it('has operators', () => {
-    expect(MathProblems.operators).toBeDefined()
+  it('has evaluator', () => {
+    expect(MathProblems.evaluator).toBeDefined()
   })
 })
