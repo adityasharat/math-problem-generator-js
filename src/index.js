ef6de708b8c7518b2e4ab389bd335c4d2c8a54f8
adityasharat <adityasharat@gmail.com>
2018-09-26 21:45:31 +0100

    Use evaluator module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import functions from './functions';
 import operators from './operators';
+import * as evaluator from './evaluator';
 
 const MathProblems = {};
 
 MathProblems.version = '1.0.0';
 MathProblems.functions = functions;
 MathProblems.operators = operators;
+MathProblems.evaluator = evaluator;
 
 export default MathProblems;
