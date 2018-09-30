b98b61be9d9c014bea8238807d11e6d7426bf909
adityasharat <adityasharat@gmail.com>
2018-09-30 21:29:37 +0100

    Add RandomStatelessGenerator

--- src/generator/index.js
+++ src/generator/index.js
@@ -1,3 +1,25 @@
-export default class Generator {
+export class Problem {
+  constructor(x, op, y) {
+    this.x = x;
+    this.op = op;
+    this.y = y;
+  }
+  toString() {
+    return this.x + this.op + this.y;
+  }
+}
 
-};
+export class RandomStatelessGenerator {
+  constructor(min, max) {
+    this.min = min;
+    this.max = max;
+    this.set = new Set();
+  }
+
+  next(op) {
+    const x = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+    const y = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+
+    return new Problem(x, op, y);
+  }
+}
