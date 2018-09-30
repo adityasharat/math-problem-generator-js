f1c776b5adfc55e96e55291b20705643f54abdfa
adityasharat <adityasharat@gmail.com>
2018-09-30 21:47:27 +0100

    update unit tests

--- src/generator/index.test.js
+++ src/generator/index.test.js
@@ -5,4 +5,9 @@ describe('RandomStatelessGenerator', () => {
     const generator = new RandomStatelessGenerator(0, 9);
     expect(generator).toBeDefined();
   })
+  it('next creates a new math problem', () => {
+    const generator = new RandomStatelessGenerator(0, 9);
+    const problem = generator.next('+');
+    expect(problem).toBeDefined();
+  })
 })
