28e6a7371857e8300e4e1085cba1105ab64f1073
adityasharat <adityasharat@gmail.com>
2018-09-30 21:30:18 +0100

    improve unit test

--- src/generator/index.test.js
+++ src/generator/index.test.js
@@ -1,9 +1,8 @@
 import { RandomStatelessGenerator } from './';
 
-describe('evalutor.isSafe', () => {
-  it('empty strings are valid', () => {
+describe('RandomStatelessGenerator', () => {
+  it('is defined', () => {
     const generator = new RandomStatelessGenerator(0, 9);
-    const problem = generator.next('+');
-    expect(problem).toBeDefined();
+    expect(generator).toBeDefined();
   })
 })
