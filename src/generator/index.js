524978b00a4bf74a2cf9b45b1061e2069251f777
adityasharat <adityasharat@gmail.com>
2018-10-01 21:53:47 +0100

    add permutation generator

--- src/generator/index.js
+++ src/generator/index.js
@@ -1,3 +1,25 @@
+function fill(min, max) {
+  return Array.from({ length: max - min + 1 }, (_, i) => i);
+}
+
+function generate(list, perm, maxLen, currLen) {
+  if (currLen === maxLen) {
+      return perm;
+  }
+  for (var i = 0, len = perm.length; i < len; i++) {
+    var currPerm = perm.shift();
+    for (var k = 0; k < list.length; k++) {
+        perm.push(currPerm.concat(list[k]));
+    }
+  }
+  return generate(list, perm, maxLen, currLen + 1);
+};
+
+function permutations(list, maxLen) {
+  var perm = list.map(val => [val]);
+  return generate(list, perm, maxLen, 1);
+}
+
 export class Problem {
   constructor(x, op, y) {
     this.x = x;
@@ -13,7 +35,6 @@ export class RandomStatelessGenerator {
   constructor(min, max) {
     this.min = min;
     this.max = max;
-    this.set = new Set();
   }
 
   next(op) {
