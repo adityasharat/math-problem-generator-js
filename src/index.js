638b0d86d815652b224c7184f8ee9222baa1b65e
adityasharat <adityasharat@gmail.com>
2018-10-04 19:21:28 +0100

    add get generator method

--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ const MathProblems = {};
 MathProblems.version = '0.1.0';
 MathProblems.evaluator = evaluator;
 
+MathProblems.getGenerator = function (level) {
+  return new SimpleGenerator(0, 9, 1);
+};
+
 export default MathProblems;
