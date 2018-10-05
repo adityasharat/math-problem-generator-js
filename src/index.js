efbdfcf7e09d65afca4b8f3066610eb65991a70e
adityasharat <adityasharat@gmail.com>
2018-10-05 17:46:17 +0100

    update Math Problems

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ MathProblems.version = '0.1.0';
 
 MathProblems.getEvaluator = () => evaluator;
 
-MathProblems.getGenerator = function (level) {
-  return new SimpleGenerator(0, 9, 1);
-};
+MathProblems.getGenerator = (level) => new SimpleGenerator(0, 9, 1);
 
 export default MathProblems;
