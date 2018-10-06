8765b94cd8e026bca3eac5c86dfd3024ac0b4077
adityasharat <adityasharat@gmail.com>
2018-10-06 15:34:50 +0100

    improve the api for generator

--- src/generators/index.js
+++ src/generators/index.js
@@ -2,40 +2,39 @@ import { fill, permutations, shuffle } from '../utils'
 import Problem from './Problem'
 
 function generate(min, max, size) {
-  return shuffle(permutations(fill(min, max), getSize(size)));
+  return shuffle(permutations(fill(min, max), size));
 }
 
 function getGeneratorParams(level) {
-  let min, max;
+  let min, max, size;
 
   switch (level) {
     case 1:
       min = 0;
       max = 9;
+      size = 2;
       break;
     case 2:
       min = 10;
       max = 99;
+      size = 2;
       break;
     default:
       min = 100;
       max = 999;
+      size = 2;
       break;
   }
 
-  return { min, max, level };
-}
-
-export function getSize(level) {
-  return level;
+  return { min, max, size, level };
 }
 
 export class SimpleGenerator {
-  constructor(min, max, level) {
+  constructor({min, max, size, level}) {
     this.min = min;
     this.max = max;
     this.level = level;
-    this.problems = generate(min, max, level);
+    this.problems = generate(min, max, size);
     this.index = 0;
   }
 
@@ -44,4 +43,9 @@ export class SimpleGenerator {
     this.index++;
     return new Problem(set[0], op, set[1]);
   }
+}
+
+export function generator(level) {
+  const params = getGeneratorParams(level);
+  return new SimpleGenerator(params);
 }
