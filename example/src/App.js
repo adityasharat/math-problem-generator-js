eeda53f0258053d75b6c96021d55928828e46420
adityasharat <adityasharat@gmail.com>
2018-10-06 15:47:58 +0100

    update app

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 
-import MathProblems from 'math-problem-generator-js'
+import MathProblems from 'math-problem-generator'
 
 export default class App extends Component {
   render () {
+    const evaluator = MathProblems.getEvaluator();
+
     return (
       <div>
         <h1>Let's Learn Some Math</h1>
           <ul>
             <li>
-              <code>2 + 2 = {MathProblems.evaluator.evaluate('2 + 2')}</code>
+              <code>2 + 2 = {evaluator.evaluate('2 + 2')}</code>
             </li>
           </ul>
         <code>version: {MathProblems.version}</code>
