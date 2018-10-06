dab3b1c9cca20890ba8698694d54dfb5f0997a6f
adityasharat <adityasharat@gmail.com>
2018-10-06 15:36:45 +0100

    update unit tests

--- src/generators/index.test.js
+++ src/generators/index.test.js
@@ -2,11 +2,11 @@ import { SimpleGenerator } from './';
 
 describe('RandomStatelessGenerator', () => {
   it('is defined', () => {
-    const generator = new SimpleGenerator(0, 9);
+    const generator = new SimpleGenerator({min: 0, max: 9, size: 2, level: 1});
     expect(generator).toBeDefined();
   })
   it('next creates a new math problem', () => {
-    const generator = new SimpleGenerator(0, 9);
+    const generator = new SimpleGenerator({min: 0, max: 9, size: 2, level: 1});
     const problem = generator.next('+');
     expect(problem).toBeDefined();
   })
@@ -14,7 +14,7 @@ describe('RandomStatelessGenerator', () => {
 
 describe('Problem', () => {
   it('is evaulated correctly', () => {
-    const generator = new SimpleGenerator(0, 9);
+    const generator = new SimpleGenerator({min: 0, max: 9, size: 2, level: 1});
     const problem = generator.next('+');
     expect(problem.evaluate()).toBe(eval(problem.toString()));
   })
