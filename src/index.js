ad25278aab04bb8808a7ddd20abb99cc4bffa0bf
adityasharat <adityasharat@gmail.com>
2018-10-07 22:43:22 +0100

    update export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as evaluator from './evaluator'
-import { create } from './generators'
+import { create, Problem } from './generators'
 
 const MathProblems = {};
 MathProblems.version = '0.1.0';
@@ -8,3 +8,7 @@ MathProblems.getEvaluator = () => evaluator;
 MathProblems.createGenerator = (level) => create(level);
 
 export default MathProblems;
+export {
+  MathProblems,
+  Problem
+}
