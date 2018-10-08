cbba9217a21850850d9be0a4457187ebcbf91927
adityasharat <adityasharat@gmail.com>
2018-10-08 19:56:39 +0100

    update app

--- example/src/App.js
+++ example/src/App.js
@@ -5,9 +5,26 @@ import { MathProblems, Problem } from 'math-problem-generator'
 class ProblemUI extends Component {
   constructor(props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
-      problem: new Problem(props.problem[0], '+', props.problem[1])
+      problem: new Problem(props.problem[0], '+', props.problem[1]),
+      check: 0
     };
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    const input = (event.target.value || '').trim();
+    const answer = this.state.problem.evaluate();
+    if (input === '' || isNaN(parseInt(input, 10))) {
+      this.setState({
+        check: 0
+      });
+    } else {
+      const num = parseInt(input, 10);
+      this.setState({
+        check: num === answer ? 1 : -1
+      })
+    }
   }
   render() {
     const problem = this.state.problem;
@@ -19,8 +36,13 @@ class ProblemUI extends Component {
           <span className="param y">{problem.y}</span>
           <span className="symbol eq">=</span>
         </div>
-        <div className="result-input">
-          <input/>
+        <form className="result-form" onSubmit={this.handleSubmit}>
+          <input type="text" name="answer" />
+          <button type="submit">Check</button>
+        </form>
+        <div>
+          {this.state.check === 1 && <div className="result-correct">correct</div>}
+          {this.state.check === -1 && <div className="result-incorrect">incorrect</div>}
         </div>
       </div>
     )
